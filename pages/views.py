c73c129f3afc88eec0ef25a3f5a3e1fd796eb71c
Svjatoslav <sderevy@wgu.edu>
2026-05-19 14:15:20 -0400

    Another minor change

--- pages/views.py
+++ pages/views.py
@@ -7,10 +7,8 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render, get_object_or_404
-from psycopg.types import none
 
 from database.models import DayWorkout, WorkoutPlan, Exercise, Workout
-
 
 # Pages
 @login_required(login_url='/login')
